82e21c7aebc8509dbe734a2ea2dd45643683b540
Gary A. Stafford <garystafford@rochester.rr.com>
2019-09-07 09:56:56 -0700

    Adding CodeBuild job

---
+++ buildspec_files/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+# validate
+sam validate --template template.yaml
+aws cloudformation validate-template \
+  --template-body file://template.yaml
+
+# build, package, deploy
+time sam build
+
+time sam package \
+    --output-template-file packaged.yaml \
+    --s3-bucket gstafford-sam-demo
+
+time sam deploy --template-file packaged.yaml \
+  --stack-name iot-dynamodb \
+  --capabilities CAPABILITY_IAM
