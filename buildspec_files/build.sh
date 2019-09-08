d1f884f72de81023b7d5b7f2edb90627f6b19586
Gary A. Stafford <garystafford@rochester.rr.com>
2019-09-07 20:15:29 -0700

    Adding CodeBuild job

--- buildspec_files/build.sh
+++ buildspec_files/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Build, package, deploy sam template
+
 #set -x
+
+export BUCKET_NAME
 
 # validate
 sam validate --template template.yaml
@@ -12,13 +16,15 @@ time sam build
 
 time sam package \
   --output-template-file packaged.yaml \
-  --s3-bucket gstafford-sam-demo
+  --s3-bucket "${BUCKET_NAME}"
 
 time sam deploy \
   --template-file packaged.yaml \
   --stack-name iot-dynamodb \
   --capabilities CAPABILITY_IAM
 
+# catch error - cloudformation returns code 255
+# (exit non-zero) if stack exists
 if [ "$?" -eq 255 ]
 then
     echo "No changes to deploy."
