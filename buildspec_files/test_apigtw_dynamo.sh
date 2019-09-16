191a9ce7677a43a3863e4f375bb560fe546b57dc
Gary A. Stafford <garystafford@rochester.rr.com>
2019-09-15 20:37:05 -0700

    New buildspec for node tests

--- buildspec_files/test_apigtw_dynamo.sh
+++ buildspec_files/test_apigtw_dynamo.sh
@@ -7,18 +7,20 @@
 
 pushd iot_api_to_dynamodb || exit
 
+ls -alh
+
 # local testing (All CRUD functions)
 sam local invoke PostMessageFunction \
-  --event iot_api_to_dynamodb/events/event_postMessage.json
+  --event events/event_postMessage.json
 
 sam local invoke GetMessageFunction \
-  --event iot_api_to_dynamodb/events/event_getMessage.json
+  --event events/event_getMessage.json
 
 sam local invoke GetMessagesFunction \
-  --event iot_api_to_dynamodb/events/event_getMessages.json
+  --event events/event_getMessages.json
 
 sam local invoke PutMessageFunction \
-  --event iot_api_to_dynamodb/events/event_putMessage.json
+  --event events/event_putMessage.json
 
 sam local invoke DeleteMessageFunction \
-  --event iot_api_to_dynamodb/events/event_deleteMessage.json
+  --event events/event_deleteMessage.json
